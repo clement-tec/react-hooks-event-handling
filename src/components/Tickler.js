bceaacd86dae9c9c59d8eb961a432be2630513c5
clement senou <clementsenou74@gmail.com>
2023-02-23 22:31:11 -0600

    complet

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
-  }
-
-  return <button>Tickle me!</button>;
+   return <button onClick={() => console.log("GBOZAAAA")}>Tickle me!</button>;
 }
 
 export default Tickler;
